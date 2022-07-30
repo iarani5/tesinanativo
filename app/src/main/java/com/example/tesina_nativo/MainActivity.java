b4d51fb6657c665739fd56ce153b9f41aa25f7d2
Iara <iaranizza5@gmail.com>
2022-07-30 17:19:25 -0300

    update form view

--- app/src/main/java/com/example/tesina_nativo/MainActivity.java
+++ app/src/main/java/com/example/tesina_nativo/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -60,4 +61,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    
+
+
 }
